4ae7e922bb7bd17e4378bd4539802047f7102151
Damon Douglas <douglas.damon@gmail.com>
2017-03-18 14:58:22 -0700

    Impl info

---
+++ lib/src/info.dart
@@ -0,0 +1,89 @@
+library flickr.info;
+
+import 'dart:async';
+import 'base.dart' as base;
+
+final _METHOD = "flickr.photos.getInfo";
+final _PARAMS = {};
+final _PHOTO_URL_BASE = 'staticflickr.com';
+final _OWNER_URL_BASE = 'www.flickr.com';
+
+final PHOTO_KEY = 'photo';
+final ID_KEY = 'id';
+final PHOTO_ID_KEY = 'photo_id';
+final OWNER_KEY = 'owner';
+final OWNER_ID_KEY = 'nsid';
+final USERNAME_KEY = 'username';
+final REALNAME_KEY = 'realname';
+final LICENSE_ID_KEY = 'license';
+final SECRET_KEY = 'secret';
+final SERVER_KEY = 'server';
+final FARM_KEY = 'farm';
+final ORIGINAL_SECRET_KEY = 'originalsecret';
+final ORIGINAL_FORMAT_KEY = 'originalformat';
+
+class Info {
+  String id;
+  String secret;
+  String server;
+  String originalSecret;
+  String originalFormat;
+  int farm;
+  int licenseId;
+  Owner owner;
+
+  Info.fromJson(Map json) {
+    json = json[PHOTO_KEY];
+    id = json[ID_KEY];
+    secret = json[SECRET_KEY];
+    server = json[SERVER_KEY];
+    farm = json[FARM_KEY];
+    originalSecret = json[ORIGINAL_SECRET_KEY];
+    originalFormat = json[ORIGINAL_FORMAT_KEY];
+    licenseId = int.parse(json[LICENSE_ID_KEY], onError: (_) => 0);
+    owner = new Owner.fromJson(json[OWNER_KEY]);
+  }
+
+  Map toJson() => {
+    PHOTO_KEY: {
+      ID_KEY: id,
+      SECRET_KEY: secret,
+      SERVER_KEY: server,
+      FARM_KEY: farm,
+      ORIGINAL_SECRET_KEY: originalSecret,
+      ORIGINAL_FORMAT_KEY: originalFormat,
+      LICENSE_ID_KEY: licenseId,
+      OWNER_ID_KEY: owner.toJson()
+    }
+  };
+
+  // https://www.flickr.com/services/api/misc.urls.html
+  Uri get url => new Uri.https('farm$farm.$_PHOTO_URL_BASE', '$server/${id}_${originalSecret}_o.${originalFormat}');
+}
+
+class Owner {
+  String id;
+  String userName;
+  String realName;
+
+  Owner.fromJson(Map json) {
+    id = json[OWNER_ID_KEY];
+    userName = json[USERNAME_KEY];
+    realName = json[REALNAME_KEY];
+  }
+
+  Map toJson() => {
+    OWNER_ID_KEY: id,
+    USERNAME_KEY: userName,
+    REALNAME_KEY: realName
+  };
+
+  Uri get url => new Uri.https(_OWNER_URL_BASE, 'people/$id');
+}
+
+Future<Map> get(String apiKey, String photoId) {
+  var client = new base.Client(apiKey);
+  return client.get(_METHOD, {
+    PHOTO_ID_KEY: photoId
+  });
+}
---
+++ test/info_test.dart
@@ -0,0 +1,145 @@
+library info_test;
+
+import 'package:test/test.dart';
+import 'package:flickr/src/info.dart' as info;
+import 'package:http/http.dart' as http;
+import 'common.dart' as common;
+
+String PHOTO_ID = '27940317290';
+String SECRET = '9842ee4acd';
+String SERVER = '8688';
+int FARM = 9;
+String LICENSE = '4';
+String ORIGINAL_SECRET = '4efa0b4164';
+String ORIGINAL_FORMAT = 'jpg';
+
+String OWNER_ID = '142142221@N02';
+String USER_NAME = 'cyoapp';
+String REAL_NAME = 'cyoapp';
+int STATUS_OK = 200;
+
+main() {
+  group('info', () {
+    Map originalResult;
+    Map result;
+    setUpAll(() async {
+      var apiKey = common.getApiKey();
+      originalResult = await info.get(apiKey, PHOTO_ID);
+      result = originalResult[info.PHOTO_KEY];
+    });
+
+    group('parses', (){
+      group('Map', () {
+        test(info.ID_KEY, (){
+          expect(result[info.ID_KEY], PHOTO_ID);
+        });
+
+        test(info.SECRET_KEY, (){
+          expect(result[info.SECRET_KEY], SECRET);
+        });
+
+        test(info.SERVER_KEY, (){
+          expect(result[info.SERVER_KEY], SERVER);
+        });
+
+        test(info.FARM_KEY, (){
+          expect(result[info.FARM_KEY], FARM);
+        });
+
+        test(info.ORIGINAL_SECRET_KEY, (){
+          expect(result[info.ORIGINAL_SECRET_KEY], ORIGINAL_SECRET);
+        });
+
+        test(info.ORIGINAL_FORMAT_KEY, (){
+          expect(result[info.ORIGINAL_FORMAT_KEY], ORIGINAL_FORMAT);
+        });
+
+        test(info.LICENSE_ID_KEY, (){
+          expect(result[info.LICENSE_ID_KEY], LICENSE);
+        });
+
+        group(info.OWNER_KEY, () {
+          Map ownerJson;
+          setUpAll(() => ownerJson = result[info.OWNER_KEY]);
+          test(info.OWNER_ID_KEY, (){
+            expect(ownerJson[info.OWNER_ID_KEY], OWNER_ID);
+          });
+
+          test(info.USERNAME_KEY, (){
+            expect(ownerJson[info.USERNAME_KEY], USER_NAME);
+          });
+
+          test(info.REALNAME_KEY, (){
+            expect(ownerJson[info.REALNAME_KEY], REAL_NAME);
+          });
+        });
+      });
+
+      group('Info', () {
+        info.Info infoObj;
+        int urlStatusCode;
+
+        setUpAll(() async {
+          infoObj = new info.Info.fromJson(originalResult);
+          var response = await http.get(infoObj.url);
+          urlStatusCode = response.statusCode;
+        });
+        test(info.ID_KEY, (){
+          expect(infoObj.id, PHOTO_ID);
+        });
+
+        test(info.SECRET_KEY, (){
+          expect(infoObj.secret, SECRET);
+        });
+
+        test(info.SERVER_KEY, (){
+          expect(infoObj.server, SERVER);
+        });
+
+        test(info.FARM_KEY, (){
+          expect(infoObj.farm, FARM);
+        });
+
+        test(info.ORIGINAL_SECRET_KEY, (){
+          expect(infoObj.originalSecret, ORIGINAL_SECRET);
+        });
+
+        test(info.ORIGINAL_FORMAT_KEY, (){
+          expect(infoObj.originalFormat, ORIGINAL_FORMAT);
+        });
+
+        test('original photo url', (){
+          expect(urlStatusCode, STATUS_OK);
+        });
+
+        group('Owner', () {
+          info.Owner owner;
+          int ownerUrlStatusCode;
+
+          // setUpAll(() => owner = infoObj.owner);
+          setUpAll(() async {
+            owner = infoObj.owner;
+            var response = await http.get(owner.url);
+            ownerUrlStatusCode = response.statusCode;
+          });
+
+          test(info.OWNER_ID_KEY, (){
+            expect(owner.id, OWNER_ID);
+          });
+
+          test(info.USERNAME_KEY, (){
+            expect(owner.userName, USER_NAME);
+          });
+
+          test(info.REALNAME_KEY, (){
+            expect(owner.realName, REAL_NAME);
+          });
+
+          test('url', (){
+            expect(ownerUrlStatusCode, STATUS_OK);
+          });
+        });
+      });
+    });
+  });
+}
